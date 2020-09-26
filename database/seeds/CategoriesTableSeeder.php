3ed1fe9ea23c7645fa2388fb2d56d325d2448ff1
ray <ray@ray.com>
2020-09-25 23:18:55 -0400

    Created the seeders

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Category;
+use Illuminate\Database\Seeder;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+
+    public function run()
+    {
+      Category::create([
+        'name' => 'Marketting'
+      ]);
+
+      Category::create([
+        'name' => 'Business'
+      ]);
+
+      Category::create([
+        'name' => 'Finance'
+      ]);
+
+      Category::create([
+        'name' => 'Entrepreneurship'
+      ]);
+
+      Category::create([
+        'name' => 'Science'
+      ]);
+
+      Category::create([
+        'name' => 'Biography'
+      ]);
+
+    }
+}
